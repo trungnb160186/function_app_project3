3acc7e4e8fb1afd1334eb1864e7065155a7bb24b
trungnb160186 <trungnbbkno1@gmail.com>
2024-05-02 22:56:41 +0700

    get attendees

--- NotificationHandle/__init__.py
+++ NotificationHandle/__init__.py
@@ -1,5 +1,6 @@
 import logging
 import psycopg2
+import os
 from azure.functions import ServiceBusMessage
 
 
@@ -8,12 +9,17 @@ def main(msg: ServiceBusMessage):
                  msg.get_body().decode('utf-8'))
     
     # Update connection string information
-    host = "<server-name>"
-    dbname = "<database-name>"
-    user = "<admin-username>"
-    password = "<admin-password>"
+    host = os.environ["DB_HOT"]
+    dbname = os.environ["DB_NAME"]
+    user = os.environ["DB_USER"]
+    password = os.environ["DB_PW"]
     sslmode = "require"
 
     # Construct connection string
-    # conn_string = "host={0} user={1} dbname={2} password={3} sslmode={4}".format(host, user, dbname, password, sslmode)
-    # conn = psycopg2.connect(conn_string) 
+    conn_string = "host={0} user={1} dbname={2} password={3} sslmode={4}".format(host, user, dbname, password, sslmode)
+    conn = psycopg2.connect(conn_string)
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM public.attendee;")
+    attendees =  cursor.fetchall()
+    for attendee in attendees:
+        print(attendee)
